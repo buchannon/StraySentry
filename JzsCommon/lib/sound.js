f3abbcdb9107d3798dc5707d344ea5f02c7b8a9d
buchannon <buchannon@gmail.com>
2015-05-19 22:10:39 -0700

    small cleanup

--- JzsCommon/lib/sound.js
+++ JzsCommon/lib/sound.js
@@ -1,7 +1,4 @@
 var utils = require('./utils');
-//var fs = require('fs');
-//var lame = require('lame');
-//var Speaker = require('speaker');
 
 console.log("jzs sounds loaded");
 
@@ -11,14 +8,4 @@ exports.MakeSound = function() {
     var filePath = '/home/pi/Desktop/Projects/StraySentry/public/sounds/lux-laugh.mp3';
 
     utils.cmd('omxplayer', [filePath]);
-    //foo = new cmd('netstat', ['-rn'],
-//    function (me, data) {me.stdout += data.toString();},
-//    function (me) {me.exit = 1;}
-//);
-
-    //fs.createReadStream('../../public/sounds/lux-laugh.mp3')
-    //    .pipe(new lame.Decoder())
-    //    .on('format', function (format) {
-    //        this.pipe(new Speaker(format));
-    //    });
 };
